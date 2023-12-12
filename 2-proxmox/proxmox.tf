2ba5241af919ad0b27f44629668cc0163dad077c
Enrique Ostúa <ostua@dte.us.es>
2023-12-12 15:12:37 +0100

    Update proxmox.tf

--- 2-proxmox/proxmox.tf
+++ 2-proxmox/proxmox.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.11"
+      source  = "TheGameProfi/proxmox"
+      version = "2.9.15"
     }
   }
 }
